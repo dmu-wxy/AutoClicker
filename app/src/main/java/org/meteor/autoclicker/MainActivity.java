b063bebc9c2c58f374f3a3cfaa552090955571b4
meteor PC <2290502632@qq.com>
2023-02-13 23:23:20 +0800

    to 悬浮窗

--- app/src/main/java/org/meteor/autoclicker/MainActivity.java
+++ app/src/main/java/org/meteor/autoclicker/MainActivity.java
@@ -3,6 +3,7 @@ package org.meteor.autoclicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 判断是否有悬浮窗权限
+        boolean canDrawOverlays = Settings.canDrawOverlays(this);
+        if(!canDrawOverlays){
+
+        }
     }
 }
